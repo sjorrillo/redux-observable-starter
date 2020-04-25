e89de09f96bfdd541204bc69de026886cfeab013
Javier Orrillo <javier.orrillo@nuorder.com>
2020-04-24 20:05:20 -0500

    add ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const config = {
   bail: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json', 'node', 'ts'],
   moduleDirectories: ['node_modules'],
   testRegex: '/__(tests|specs)__/.*.([\\.test\\.spec])\\.(j|t)s$',
---
+++ src/common/__tests__/mediator-test.ts
@@ -0,0 +1,19 @@
+import { mediator, EventType } from '../modules/mediator';
+
+describe('modules/mediator', () => {
+  describe('when a we have a subscription to an event', () => {
+    it('should listen any value that is emitted', () => {
+      const subscriptionHandler = jest.fn(() => {});
+      mediator.on(EventType.Login, {
+        next: subscriptionHandler,
+      });
+
+      mediator.emit(EventType.Login, '1');
+      mediator.emit(EventType.Login, '2');
+      mediator.emit(EventType.Login, '3');
+
+      expect(subscriptionHandler).toBeCalledTimes(3);
+      expect([].concat(...subscriptionHandler.mock.calls)).toEqual(['1', '2', '3']);
+    });
+  });
+});
